25ed70599b63c01123b3be3d2e98e5744879d3b5
Dotoria <h71148@gmail.com>
2024-09-03 22:34:10 +0900

    add ball

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Arrow : MonoBehaviour
 {
     private Player player;
+    public GameObject ball;
+    
     public bool canDirect = true;
     public Vector2 direction;
+    public float activeAngle = 80f;
     
     void Awake()
     {
@@ -26,15 +24,19 @@ public class Arrow : MonoBehaviour
                 mousePosition.z = 0;
                 
                 direction = (mousePosition - transform.position).normalized;
-                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
 
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+                angle = Mathf.Clamp(angle, -activeAngle, activeAngle);
+                
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
             }
             
             if (Input.GetMouseButtonUp(0))
             {
                 canDirect = !canDirect;
                 player.canDrag = !player.canDrag;
+                
+                Instantiate(ball, transform.position, transform.rotation);
                 gameObject.SetActive(false);
             }
         }
