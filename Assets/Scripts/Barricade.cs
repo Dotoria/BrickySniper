f3cace159b30ab0973ce37d026f55f761e891b15
Dotoria <h71148@gmail.com>
2024-11-07 11:21:50 +0900

    barricade 체력바 적용

--- Assets/Scripts/Barricade.cs
+++ Assets/Scripts/Barricade.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Barricade : MonoBehaviour
 {
+    public GameObject healthBar;
+    private Slider _slider;
+    private TextMeshProUGUI _text;
+    
     public int maxHealthPoint = 100;
     public int currentHealthPoint;
     private BoxCollider2D _collider;
     
     void Awake()
     {
+        _slider = healthBar.GetComponent<Slider>();
+        _text = healthBar.GetComponentInChildren<TextMeshProUGUI>();
+
         currentHealthPoint = maxHealthPoint;
         _collider = GetComponent<BoxCollider2D>();
+        
+        _slider.value = maxHealthPoint;
+        _text.text = $"{currentHealthPoint} / {maxHealthPoint}";
     }
 
     void Update()
@@ -25,6 +37,9 @@ public class Barricade : MonoBehaviour
         {
             EnemyManager.Instance.DestroyEnemy(other.gameObject);
             currentHealthPoint -= other.GetComponent<Enemy>().attackPoint;
+
+            _slider.value = currentHealthPoint * 1f / maxHealthPoint;
+            _text.text = $"{currentHealthPoint} / {maxHealthPoint}";
         }
     }
 }
